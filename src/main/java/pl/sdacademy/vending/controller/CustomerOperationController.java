21f3bf4cc4d97763595e8afb365c1face133aa2b
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-02 11:44:33 +0200

    Made new methods from loops

--- src/main/java/pl/sdacademy/vending/controller/CustomerOperationController.java
+++ src/main/java/pl/sdacademy/vending/controller/CustomerOperationController.java
@@ -10,21 +10,35 @@ public class CustomerOperationController {
     }
 
     public void printMachine() {
-        for (int rows = 0; rows < machine.rowsSize(); rows++) {
-            for (int cols = 0; cols < machine.colsSize(); cols++) {
-                System.out.print("+--------+");
+        for (int row = 0; row < machine.rowsSize(); row++) {
+            for (int col = 0; col < machine.colsSize(); col++) {
+                printUpperBoundaryForCell(row, col);
             }
             System.out.println();
-            for (int cols = 0; cols < machine.colsSize(); cols++) {
-                char rowSymbol = (char) ('A' + rows);
-                int colSymbol = cols + 1;
-                System.out.print("|   " + rowSymbol + colSymbol + "   |");
+
+            for (int col = 0; col < machine.colsSize(); col++) {
+                printSymbolForCell(row, col);
             }
             System.out.println();
-            for (int cols = 0; cols < machine.colsSize(); cols++) {
-                System.out.print("+--------+");
+
+            for (int col = 0; col < machine.colsSize(); col++) {
+                printLowerBoundaryForCell(row, col);
             }
             System.out.println();
         }
+    }
+
+    private void printUpperBoundaryForCell(int row, int col) {
+        System.out.print("+--------+");
+    }
+
+    private void printSymbolForCell(int row, int col) {
+        char rowSymbol = (char) ('A' + row);
+        int colSymbol = col + 1;
+        System.out.print("|   " + rowSymbol + colSymbol + "   |");
+    }
+
+    private void printLowerBoundaryForCell(int row, int col) {
+        System.out.print("+--------+");
     }
 }
