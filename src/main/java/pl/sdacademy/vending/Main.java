e76883189a0e7e70f71dfa462582aa557916bf92
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-02 12:52:25 +0200

    Added application.properties

--- src/main/java/pl/sdacademy/vending/Main.java
+++ src/main/java/pl/sdacademy/vending/Main.java
@@ -1,9 +1,11 @@
 package pl.sdacademy.vending;
 
 import pl.sdacademy.vending.controller.CustomerOperationController;
+import pl.sdacademy.vending.util.Configuration;
 
 public class Main {
     public static void main(String[] args) {
         new CustomerOperationController().printMachine();
+        new Configuration();
     }
 }
