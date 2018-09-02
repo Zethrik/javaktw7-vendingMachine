d7f5b6b624bf07bf3e3f488ffe5a92858cf5ebee
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-02 13:56:42 +0200

    Added ConfigurationTest class

---
+++ src/test/java/ConfigurationTest.java
@@ -0,0 +1,64 @@
+import org.junit.Before;
+import org.junit.Test;
+import pl.sdacademy.vending.util.Configuration;
+
+import static org.junit.Assert.assertEquals;
+
+public class ConfigurationTest {
+
+    private Configuration configuration;
+
+    @Before
+    public void executedBeforeEach() {
+        configuration = new Configuration();
+    }
+
+    @Test
+    public void shouldReturnTextPropertyFromConfiguration() {
+        // given
+        String propertyName = "property.text.value";
+
+        // when
+        String retrievedValue = configuration.getProperty(propertyName, "DEFEAULT_VALUE");
+
+        // then
+        assertEquals("some text", retrievedValue);
+    }
+
+    @Test
+    public void shouldReturnLongPropertyFromConfiguration() {
+        // given
+        String propertyName = "property.long.value";
+
+        // when
+        Long retrievedValue = configuration.getProperty(propertyName, 15L);
+
+        // then
+        assertEquals((Long)1337L, retrievedValue);
+    }
+
+    @Test
+    public void shouldReturnDefeaultTextPropertyFromConfiguration() {
+        // given
+        String propertyName = "";
+
+        // when
+        String retrievedValue = configuration.getProperty(propertyName, "DEFEAULT_VALUE");
+
+        // then
+        assertEquals("DEFEAULT_VALUE", retrievedValue);
+    }
+
+    @Test
+    public void shouldReturnDefeaultLongPropertyFromConfiguration() {
+        // given
+        String propertyName = "";
+
+        // when
+        Long retrievedValue = (configuration.getProperty(propertyName, 15L));
+
+        // then
+        assertEquals((Long)15L, retrievedValue);
+    }
+
+}
