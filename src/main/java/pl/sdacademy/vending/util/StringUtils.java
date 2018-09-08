712e10c8695b53d15dca7af7b33601eaa881ce90
Bartłomiej Jonda <bartlomiej.jonda@gmail.com>
2018-09-08 15:37:07 +0200

    Added adjustPrice method to the StringUtils class

--- src/main/java/pl/sdacademy/vending/util/StringUtils.java
+++ src/main/java/pl/sdacademy/vending/util/StringUtils.java
@@ -4,21 +4,49 @@ public class StringUtils {
     public static String adjustText(String textToMatch, Integer expectedTextLength) {
         if (textToMatch.length() < expectedTextLength) {
             int requiredSpaces = expectedTextLength - textToMatch.length();
-            int spaciesOnLeft = (requiredSpaces + 1) / 2;
-            int spaciesOnRight = requiredSpaces / 2;
-            return multiplyText(" ", spaciesOnLeft)
+            int spacesOnLeft = (requiredSpaces + 1) / 2;
+            int spacesOnRight = requiredSpaces / 2;
+            return multiplyText(" ", spacesOnLeft)
                     + textToMatch
-                    + multiplyText(" ", spaciesOnRight);
+                    + multiplyText(" ", spacesOnRight);
         } else {
             return textToMatch.substring(0, expectedTextLength);
         }
     }
 
-    public static String multiplyText (String textToMultiply, Integer times) {
+    public static String multiplyText(String textToMultiply, Integer times) {
         StringBuilder stringBuilder = new StringBuilder();
         for (Integer count = 0; count < times; count++) {
             stringBuilder = stringBuilder.append(textToMultiply);
         }
         return stringBuilder.toString();
+    }
+
+    public static String adjustPrice(Long price) {
+        String priceToText = "";
+        if (price > 99) {
+            priceToText = price.toString();
+        } else if (price < 10) {
+            priceToText = "00" + price.toString();
+        } else {
+            priceToText = "0" + price.toString();
+        }
+
+
+        String priceToSplit = priceToText.substring(0, priceToText.length() - 2);
+        String splittedText = "";
+        int counter = 0;
+
+        for (int i = priceToSplit.length() - 1; i >= 0; i--) {
+            if (counter != 3) {
+                splittedText = String.valueOf(priceToSplit.charAt(i)) + splittedText;
+                counter++;
+            } else {
+                splittedText = String.valueOf(priceToSplit.charAt(i)) + " " + splittedText;
+                counter = 1;
+            }
+        }
+
+        return splittedText + "," + priceToText.substring(priceToText.length() - 2);
     }
 }
--- src/test/java/pl/sdacademy/vending/util/StringUtilsTest.java
+++ src/test/java/pl/sdacademy/vending/util/StringUtilsTest.java
@@ -57,4 +57,70 @@ public class StringUtilsTest {
         // then
         assertEquals("   Ala  ", adjustedText);
     }
+
+    @Test
+    public void shouldAddCommaToCorrectPrice() {
+        // given
+        Long priceToAdjust1 = 123L;
+        Long priceToAdjust2 = 12L;
+        Long priceToAdjust3 = 3L;
+
+        // when
+        String adjustedPrice1 = StringUtils.adjustPrice(priceToAdjust1);
+        String adjustedPrice2 = StringUtils.adjustPrice(priceToAdjust2);
+        String adjustedPrice3 = StringUtils.adjustPrice(priceToAdjust3);
+
+        // then
+        assertEquals("1,23", adjustedPrice1);
+        assertEquals("0,12", adjustedPrice2);
+        assertEquals("0,03", adjustedPrice3);
+    }
+
+    @Test
+    public void shouldSplitThousandsAndHundreds() {
+        // given
+        Long priceToAdjust = 123456L;
+
+        // when
+        String adjustedPrice = StringUtils.adjustPrice(priceToAdjust);
+
+        // then
+        assertEquals("1 234,56", adjustedPrice);
+    }
+
+    @Test
+    public void shouldSplitThousandsAndHundreds1() {
+        // given
+        Long priceToAdjust = 12345L;
+
+        // when
+        String adjustedPrice = StringUtils.adjustPrice(priceToAdjust);
+
+        // then
+        assertEquals("123,45", adjustedPrice);
+    }
+
+    @Test
+    public void shouldSplitThousandsAndHundreds0() {
+        // given
+        Long priceToAdjust = 0L;
+
+        // when
+        String adjustedPrice = StringUtils.adjustPrice(priceToAdjust);
+
+        // then
+        assertEquals("0,00", adjustedPrice);
+    }
+
+    @Test
+    public void shouldSplitBillionsMillionsAndThousands() {
+        // given
+        Long priceToAdjust = 1234567890123456789L;
+
+        // when
+        String adjustedPrice = StringUtils.adjustPrice(priceToAdjust);
+
+        // then
+        assertEquals("12 345 678 901 234 567,89", adjustedPrice);
+    }
 }
